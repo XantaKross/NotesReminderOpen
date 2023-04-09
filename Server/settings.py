b2d3770530864142f875c0b8428fb3147f4b3013
Shahjahan Mujeeb <shahjahankhan321@gmail.com>
2023-04-09 15:31:48 +0530

    Added files and changes required for deployment

--- Server/settings.py
+++ Server/settings.py
@@ -19,11 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-czzxlm_wcsl8rk0aorx_k^ot2yd6=3_%1o$^q)y!#owigglq#a'
+# SECURITY WARNING: keep the secret key.txt used in production secret!
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -132,7 +134,7 @@ STATICFILES_DIRS = (
 )
 
 
-# Default primary key field type
+# Default primary key.txt field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
@@ -140,3 +142,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # to make sure django uses my custom defined authentication model to handle all
 # login and sign in tasks.
 AUTH_USER_MODEL = "Accounts.CustomUser"
+
+
